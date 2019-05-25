78ff9a3628566d96f05ff3fbc335d201d0f227d7
Umut Sirin <umutsirin1@gmail.com>
2019-05-25 23:16:42 +0000

    Add react specific config
    use it as { "extends": ["@usirin/eslint-config",
    "@usirin/eslint-config/react"] }

---
+++ react.js
@@ -0,0 +1,11 @@
+module.exports = {
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect"
+    }
+  },
+  extends: ["plugin:react/recommended", "prettier/react"],
+  plugins: ["react"],
+  rules: {}
+};
